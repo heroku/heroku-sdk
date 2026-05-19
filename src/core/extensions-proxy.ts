7bc79ac5766f6f9dfd76e5b1d3b85963fb462b09
Timothy Lowrimore <tlowrimore@salesforce.com>
2026-05-18 18:40:44 -0600

    feat(core): add mergeExtensions proxy that overlays extensions on routes

---
+++ src/core/extensions-proxy.ts
@@ -0,0 +1,51 @@
+import createDebug from 'debug'
+
+import type {ResourceCtx, ResourceExtension, ResourceMethods} from './extend-resource.js'
+
+const debug = createDebug('heroku:sdk:extensions')
+
+export function mergeExtensions<T extends object>(
+  routesProxy: T,
+  extensions: readonly ResourceExtension[],
+  ctx: ResourceCtx,
+): T {
+  const methodsByResource = new Map<string, ResourceMethods>()
+
+  for (const ext of extensions) {
+    const merged = methodsByResource.get(ext.resource) ?? {}
+    Object.assign(merged, ext.factory(ctx))
+    methodsByResource.set(ext.resource, merged)
+  }
+
+  for (const [resource, methods] of methodsByResource) {
+    const routeResource = (routesProxy as Record<string, unknown>)[resource]
+    if (routeResource && typeof routeResource === 'object') {
+      for (const methodName of Object.keys(methods)) {
+        if (methodName in routeResource) {
+          debug('extension shadows upstream route: %s.%s', resource, methodName)
+        }
+      }
+    }
+  }
+
+  return new Proxy(routesProxy, {
+    get(target, resourceKey: string, receiver) {
+      const extMethods = methodsByResource.get(resourceKey)
+      const routeResource = Reflect.get(target, resourceKey, receiver)
+
+      if (!extMethods) {
+        return routeResource
+      }
+
+      return new Proxy(routeResource ?? {}, {
+        get(routeTarget, methodKey: string, methodReceiver) {
+          if (Object.hasOwn(extMethods, methodKey)) {
+            return extMethods[methodKey]
+          }
+
+          return Reflect.get(routeTarget, methodKey, methodReceiver)
+        },
+      })
+    },
+  })
+}
